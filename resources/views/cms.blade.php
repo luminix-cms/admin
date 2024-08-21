8f780b332f951bb5b72cc26af206e48d73797db1
Bruno Polo <brunopolo@poli.ufrj.br>
2024-08-21 01:04:18 -0300

    added more translations // loading cms from unpkg

--- resources/views/cms.blade.php
+++ resources/views/cms.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <script type="module" crossorigin src="/vendor/luminix/cms/mui-cms.js"></script>
-    <link rel="stylesheet" crossorigin href="/vendor/luminix/cms/style.css">
+    <script type="module" crossorigin src="https://unpkg.com/@luminix/mui-cms@0.0.1-beta.1/bundle/mui-cms.bundle.iife.js"></script>
+    <link rel="stylesheet" crossorigin href="https://unpkg.com/@luminix/mui-cms@0.0.1-beta.1/bundle/style.css">
 </head>
 <body>
     @luminixEmbed()
