7f147c1b1f7ec3170ccd7052cd0b5e1a03939265
Bruno Polo <brunopolo@poli.ufrj.br>
2024-11-22 22:23:10 -0300

    bump: update CMS version to 0.1.0

--- src/AdminServiceProvider.php
+++ src/AdminServiceProvider.php
@@ -10,7 +10,7 @@ use Luminix\Frontend\Services\BootService;
 class AdminServiceProvider extends ServiceProvider
 {
 
-    const CMS_VERSION = '0.0.1-beta.8';
+    const CMS_VERSION = '0.1.0';
 
 
     public function register()
